ea2271fc871f166ef4dbf05a042f962d5c0be132
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-03-04 14:58:27 +0000

    Initial commit

---
+++ js.js
@@ -0,0 +1,9 @@
+function Sumit(User, pass, IMGURL){
+  if(User == "thecoderell.github.io"){
+    if(pass == "pers_PPP"){
+      if(IMGURL == "https://lh4.googleusercontent.com/-vIVmnLrBilM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuckxYVmglW4W6cTheeMbT4P9PrcLDQ/s96-c/photo.jpg"){
+        window.location = "/MEEMEAA";
+      }
+    }
+  }
+}
